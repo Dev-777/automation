f26d149600d639b9a69aa207635305ecb4cea443
arsen172317@gmail.com <arsen172317@gmail.com>
2020-08-01 15:27:55 +0400

    Start0

---
+++ src/app/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Price } from "../components/price/price";
+
+function App() {
+  return (
+    <div className="App">
+      <Price />
+    </div>
+  );
+}
+
+export default App;
