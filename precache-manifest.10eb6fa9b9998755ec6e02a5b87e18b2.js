f7a39778dc8871a250f9ff7f3575dd6d52facb0d
Arsen <arsen172317@gmail.com>
2020-08-03 22:55:17 +0400

    Updates

---
+++ precache-manifest.10eb6fa9b9998755ec6e02a5b87e18b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40ef13e89edbc87c70e3d0f488f01d5f",
+    "url": "/automation/index.html"
+  },
+  {
+    "revision": "d1e39f07f58c9f9158ae",
+    "url": "/automation/static/css/main.d9fd67d9.chunk.css"
+  },
+  {
+    "revision": "a2419462ddf791bd56ca",
+    "url": "/automation/static/js/2.1d7349dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/automation/static/js/2.1d7349dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1e39f07f58c9f9158ae",
+    "url": "/automation/static/js/main.f3a6909e.chunk.js"
+  },
+  {
+    "revision": "90ceae759533e1e22451",
+    "url": "/automation/static/js/runtime-main.1c6b32ff.js"
+  }
+]);
